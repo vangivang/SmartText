16c37e526ab5f2e4b2a3750b76e5cced4b0a1afa
Alon Minski <minskia@autodesk.com>
2014-08-27 00:26:00 +0300

    Closing keyboard when popup icon clicked

--- app/src/main/java/com/msgme/msgme/PersonMessagesActivity.java
+++ app/src/main/java/com/msgme/msgme/PersonMessagesActivity.java
@@ -32,6 +32,7 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -433,6 +434,7 @@ public class PersonMessagesActivity extends Activity {
                             @Override
                             public void onClick(View v) {
                                 //TODO: open pop up view animatedly
+                                closeKeyboard(mPopUpButton);
                                 Toast.makeText(PersonMessagesActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -473,6 +475,12 @@ public class PersonMessagesActivity extends Activity {
 
             }
         });
+    }
+
+    private void closeKeyboard(View closingView) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(closingView.getWindowToken(), 0);
     }
 
     private void addToSentContenProvider(String phoneNumber, String message) {
